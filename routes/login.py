ee54ea93e8d7c0c429003c353928e284729cea29
ItsRaelx <kontakt@itsraelx.com>
2024-10-23 20:55:11 +0200

    login fix

--- routes/login.py
+++ routes/login.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel, Field
 
@@ -15,12 +15,14 @@ class CallbackModel(BaseModel):
     code: str = Field(..., pattern=r'^[A-Za-z0-9]{30}$')
     state: str = Field(..., max_length=1024, pattern=r'^[-A-Za-z0-9+/]*={0,3}$')
 
+
 @login_router.get("/oauth2")
-async def login(state_data: StateModel):
+async def login(state_data: StateModel = Depends()):
     return RedirectResponse(url=get_oauth_url(state_data.state))
 
+
 @login_router.get("/callback")
-async def callback(callback_data: CallbackModel):
+async def callback(callback_data: CallbackModel = Depends()):
     data = await get_code(callback_data.code)
     if data is None:
         raise HTTPException(status_code=400, detail="Failed to get access token")
